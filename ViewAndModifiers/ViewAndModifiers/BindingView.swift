4b184b535e8a5233dc24b7e46bffa39e10ea6466
Sreekutty Maya <sreekuttystar@gmail.com>
2024-04-16 13:24:29 +0400

    binding

---
+++ ViewAndModifiers/ViewAndModifiers/BindingView.swift
@@ -0,0 +1,38 @@
+//
+//  BindingView.swift
+//  ViewAndModifiers
+//
+//  Created by Sreekutty Maya on 16/04/2024.
+//
+
+import SwiftUI
+
+struct BindingView: View {
+    @State private var agreedToTerms = false
+    @State private var agreedToPrivacyPolicy = false
+    @State private var agreedToEmails = false
+
+    var body: some View {
+        let agreedToAll = Binding<Bool>(
+            get: {
+                agreedToTerms && agreedToPrivacyPolicy && agreedToEmails
+            },
+            set: {
+                agreedToTerms = $0
+                agreedToPrivacyPolicy = $0
+                agreedToEmails = $0
+            }
+        )
+
+        return VStack {
+            Toggle("Agree to terms", isOn: $agreedToTerms)
+            Toggle("Agree to privacy policy", isOn: $agreedToPrivacyPolicy)
+            Toggle("Agree to receive shipping emails", isOn: $agreedToEmails)
+            Toggle("Agree to all", isOn: agreedToAll)
+        }
+    }
+}
+
+#Preview {
+    BindingView()
+}
