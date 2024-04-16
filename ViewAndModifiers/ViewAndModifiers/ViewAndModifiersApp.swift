f33e728b9b878fa5895ccc3ec8defab462293204
Sreekutty Maya <sreekuttystar@gmail.com>
2024-04-16 08:36:47 +0400

    project setup added

---
+++ ViewAndModifiers/ViewAndModifiers/ViewAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewAndModifiersApp.swift
+//  ViewAndModifiers
+//
+//  Created by Sreekutty Maya on 16/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct ViewAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
